47a242b5bdc09fe7abdca87323799d0f9c74f450
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-06-02 06:36:17 +0200

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import Home from './routes/Home';
+import Categories from './routes/Categories';
+import NotMatch from './routes/NotMatch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/*" element={<NotMatch />} />
+      </Route>
+    </Routes>
   );
 }
 
