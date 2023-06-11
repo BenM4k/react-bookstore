bac99b983d936d0b2d9a121847393f0378ffc1bc
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-06-11 19:56:46 +0200

    Reducers and actions

---
+++ src/redux/slices/books/bookSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// const myBooks = [
+//     {
+//       title: 'Fantastic Beasts And Where To find them',
+//       author: 'J. K. Rowling',
+//       category: 'action',
+//       chapter: 'Chapter 17',
+//     },
+//     {
+//       title: 'The Hunger games',
+//       author: 'Suzanne Collins',
+//       category: 'action',
+//       chapter: 'Chapter 3',
+//     },
+//     {
+//       title: 'Jurassic Parc',
+//       author: 'Michael Crichton',
+//       category: 'sci-fi',
+//       chapter: 'Introduction',
+//     },
+// ];
+
+const bookSlice = createSlice({
+    name: 'book',
+    initialState: [],
+    reducers: {
+        deleteBook: (state, action) => {
+            const bookTitle = action.payload;
+            state.myBooks = state.myBooks.filter((book) => book.title !== bookTitle )
+        },
+        addBook: (state, action) => {
+            const book = action.payload;
+            state.myBooks.push(book);
+        }
+    }
+})
+
+export const { addBook, deleteBook } = bookSlice.actions;
+export default bookSlice.reducer;
