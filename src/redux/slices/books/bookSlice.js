b241f036174385e9809c1e70dd2b3d08c0bbab3a
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-06-12 12:09:56 +0200

    Fix linters errors

--- src/redux/slices/books/bookSlice.js
+++ src/redux/slices/books/bookSlice.js
@@ -22,19 +22,19 @@ import { createSlice } from '@reduxjs/toolkit';
 // ];
 
 const bookSlice = createSlice({
-    name: 'book',
-    initialState: [],
-    reducers: {
-        deleteBook: (state, action) => {
-            const bookTitle = action.payload;
-            state.myBooks = state.myBooks.filter((book) => book.title !== bookTitle )
-        },
-        addBook: (state, action) => {
-            const book = action.payload;
-            state.myBooks.push(book);
-        }
-    }
-})
+  name: 'book',
+  initialState: [],
+  reducers: {
+    deleteBook: (state, action) => {
+      const bookTitle = action.payload;
+      state.myBooks = state.myBooks.filter((book) => book.title !== bookTitle);
+    },
+    addBook: (state, action) => {
+      const book = action.payload;
+      state.myBooks.push(book);
+    },
+  },
+});
 
 export const { addBook, deleteBook } = bookSlice.actions;
 export default bookSlice.reducer;
