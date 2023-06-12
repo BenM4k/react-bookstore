21a847fc414bad5fe47b6f3e848b77271c50cd24
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-06-12 12:35:25 +0200

    Update bookSlice.js

--- src/redux/slices/books/bookSlice.js
+++ src/redux/slices/books/bookSlice.js
@@ -1,26 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// const myBooks = [
-//     {
-//       title: 'Fantastic Beasts And Where To find them',
-//       author: 'J. K. Rowling',
-//       category: 'action',
-//       chapter: 'Chapter 17',
-//     },
-//     {
-//       title: 'The Hunger games',
-//       author: 'Suzanne Collins',
-//       category: 'action',
-//       chapter: 'Chapter 3',
-//     },
-//     {
-//       title: 'Jurassic Parc',
-//       author: 'Michael Crichton',
-//       category: 'sci-fi',
-//       chapter: 'Introduction',
-//     },
-// ];
-
 const bookSlice = createSlice({
   name: 'book',
   initialState: [],
